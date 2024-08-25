a32927db301233988c9aca2fbf82d1c82a5f6359
s3xmaxman <badside3919@gmail.com>
2024-08-26 02:40:08 +0900

    update _layout.tsx and app/index.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
 import { Stack } from "expo-router";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "react-native";
 
-export default function RootLayout() {
+const InitialLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
+
+const RootLayoutNav = () => {
+  return (
+    <ActionSheetProvider>
+      <>
+        <StatusBar style="light" />
+        <GestureHandlerRootView>
+          <InitialLayout />
+        </GestureHandlerRootView>
+      </>
+    </ActionSheetProvider>
+  );
+};
+
+export default RootLayoutNav;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,102 @@
-import { Text, View } from "react-native";
+import { Colors } from "@/constants/Colors";
+import { ModalType } from "@/types/enums";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import * as WebBrowser from "expo-web-browser";
 
 export default function Index() {
+  const { top } = useSafeAreaInsets();
+  const showModal = async (type: ModalType) => {};
+  const openLink = async () => {
+    WebBrowser.openBrowserAsync("https://github.com/s3xmaxman");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World Coding with Expo</Text>
+    <View style={[styles.container, { paddingTop: top + 30 }]}>
+      <Image
+        source={require("@/assets/images/login/trello.png")}
+        style={styles.image}
+      />
+      <Text style={styles.introText}>
+        チームワークを進めよう - どこにいても
+      </Text>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity
+          style={[styles.btn, { backgroundColor: "white" }]}
+          onPress={() => showModal(ModalType.Login)}
+        >
+          <Text style={[styles.btnText, { color: Colors.primary }]}>
+            ログイン
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.btn, { backgroundColor: "white" }]}
+          onPress={() => showModal(ModalType.SignUp)}
+        >
+          <Text style={[styles.btnText, { color: Colors.primary }]}>
+            新規登録
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.description}>
+          新規登録することで、あなたは{" "}
+          <Text style={styles.link} onPress={openLink}>
+            ユーザー通知
+          </Text>
+          および{" "}
+          <Text style={styles.link} onPress={openLink}>
+            プライバシーポリシー
+          </Text>
+          に同意したことになります。
+        </Text>
+        <Text style={styles.link}>ログインまたは新規登録ができない場合</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.primary,
+    alignItems: "center",
+  },
+  image: {
+    height: 450,
+    paddingHorizontal: 40,
+    resizeMode: "contain",
+  },
+  introText: {
+    fontWeight: "600",
+    color: "white",
+    fontSize: 17,
+    padding: 30,
+  },
+  bottomContainer: {
+    width: "100%",
+    paddingHorizontal: 40,
+    gap: 10,
+  },
+  btn: {
+    padding: 10,
+    borderRadius: 8,
+    alignItems: "center",
+    borderColor: "#fff",
+    borderWidth: 1,
+  },
+  btnText: {
+    fontSize: 18,
+  },
+  description: {
+    paddingTop: 30,
+    fontSize: 12,
+    textAlign: "center",
+    color: "#fff",
+    marginHorizontal: 60,
+  },
+  link: {
+    color: "#fff",
+    fontSize: 12,
+    textAlign: "center",
+    textDecorationLine: "underline",
+  },
+});
