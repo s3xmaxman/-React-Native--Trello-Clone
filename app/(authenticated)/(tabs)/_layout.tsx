4e630a431d18d4c2b4326dd6c6f86a05e4190fb1
s3xmaxman <badside3919@gmail.com>
2024-08-27 01:23:12 +0900

    update _layout.tsx

---
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -0,0 +1,7 @@
+import { Tabs } from "expo-router";
+
+const Layout = () => {
+  return <Tabs />;
+};
+
+export default Layout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,12 @@
-import { Redirect, Stack, useRouter } from "expo-router";
+import { Redirect, Stack, useRouter, useSegments } from "expo-router";
 import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { StatusBar } from "react-native";
+import { ActivityIndicator, StatusBar, View } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
 import { useEffect } from "react";
 import { SupabaseProvider } from "@/context/SupabaseContext";
+import { Colors } from "@/constants/Colors";
 
 const tokenCache = {
   async getToken(key: string) {
@@ -25,6 +26,26 @@ const tokenCache = {
 };
 
 const InitialLayout = () => {
+  const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+  const segments = useSegments();
+
+  useEffect(() => {
+    if (!isLoaded) {
+      return;
+    }
+
+    const inAuthGroup = segments[0] === "(authenticated)";
+  }, [isSignedIn]);
+
+  if (!isLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
   return (
     <SupabaseProvider>
       <Stack>
