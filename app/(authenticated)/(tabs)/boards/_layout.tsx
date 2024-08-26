5b163596161f3da5938f319176292f3784b352a8
s3xmaxman <badside3919@gmail.com>
2024-08-27 03:40:39 +0900

    update _layout.tsx

--- app/(authenticated)/(tabs)/boards/_layout.tsx
+++ app/(authenticated)/(tabs)/boards/_layout.tsx
@@ -1,9 +1,55 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Stack } from "expo-router";
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
+import { Image, TouchableOpacity } from "react-native";
 
 const Layout = () => {
-  return <Stack />;
-};
+  const router = useRouter();
 
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerStyle: {
+            backgroundColor: Colors.primary,
+          },
+          headerTitle: () => (
+            <Image
+              style={{ width: 120, height: 50, resizeMode: "contain" }}
+              source={require("@/assets/images/trello-logo-gradient-white.png")}
+            />
+          ),
+        }}
+      />
+      {/* <Stack.Screen
+        name="new-board"
+        options={{
+          headerShown: false,
+          presentation: "modal",
+        }}
+      /> */}
+
+      {/* <Stack.Screen
+        name="templates"
+        options={{
+          title: "Start with a template",
+          presentation: "fullScreenModal",
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: "#E3DFE9",
+                borderRadius: 16,
+                padding: 6,
+              }}
+            >
+              <Ionicons name="close" size={18} color={"#716E75"} />
+            </TouchableOpacity>
+          ),
+        }}
+      /> */}
+    </Stack>
+  );
+};
 export default Layout;
